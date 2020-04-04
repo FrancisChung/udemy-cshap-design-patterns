7044df5830fa1b9f00b9211e25e2f93f7e352754
Francis Chung <francis.c.chung@gmail.com>
2020-04-04 11:41:25 +1100

    Add project files.

---
+++ MediatorChatRoom/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediatorChatRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        public string Name;
+        public ChatRoom room;
+        private List<string> chatLog = new List<string>();
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void Say(string message)
+        { }
+
+        public void PrivateMessage(string who, string message)
+        { }
+
+        public void Receive(string sender, string message)
+        {
+            string s = $"{sender}: {message}";
+            chatLog.Add(s);
+            Console.WriteLine($"[{Name}'s chat session] {s}");
+        }
+        
+    }
+
+    public class ChatRoom
+    {
+
+    }
+}
