8f4991a7cd200a95a8c6f0390913e0d3b49fa75f
Francis Chung <francis.c.chung@gmail.com>
2020-04-06 18:02:02 +1000

    Mediatr Demo

--- MediatrDemo/Program.cs
+++ MediatrDemo/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using JetBrains.Annotations;
+using MediatR;
 
 namespace MediatrDemo
 {
+    // ping & pong classes
+
+    public class PingCommand : IRequest<PongResponse>
+    { }
+
+    public class PongResponse
+    {
+        public DateTime Timestamp;
+
+        public PongResponse(DateTime timestamp)
+        {
+            Timestamp = timestamp;
+        }
+    }
+     
+    [UsedImplicitly]
+    public class PingCommandHandler : IRequestHandler<PingCommand, PongResponse>
+    {
+        public async Task<PongResponse> Handle(PingCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new PongResponse(DateTime.UtcNow))
+                .ConfigureAwait(false);
+        }
+    }
+
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Mediator>()
+                .As<IMediator>()
+                .InstancePerLifetimeScope();
+
+            builder.Register<ServiceFactory>(ctx =>
+            {
+                var c = ctx.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            builder.RegisterAssemblyTypes(typeof(Program).Assembly)
+                .AsImplementedInterfaces();
+
+            var container = builder.Build();
+
+            var mediator = container.Resolve<IMediator>();
+            var response = await mediator.Send(new PingCommand());
+
+            Console.WriteLine($"We got a response at {response.Timestamp}");
         }
     }
 }
